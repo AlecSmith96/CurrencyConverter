b1a216198cd3b7b8cef34e6a4a490cb5e466273b
Alec <Smith>
2019-12-29 19:24:47 +0000

    Set up index page at "/"

--- src/main/java/edu/util/currencyconverter/Controller.java
+++ src/main/java/edu/util/currencyconverter/Controller.java
@@ -1,4 +1,17 @@
 package edu.util.currencyconverter;
 
-public class Controller {
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@Slf4j
+public class Controller
+{
+  @GetMapping("/")
+  public ModelAndView getIndexPage()
+  {
+      return new ModelAndView("converter");
+  }
 }
